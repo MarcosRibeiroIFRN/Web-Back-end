25764400b48f20e9510adf8babb8bc56b22a9fae
MARCOS VICTOR DE ANDRADE RIBEIRO <20231148060017@ifrn.local>
2024-04-02 14:31:43 -0300

    autenticação por jwt

--- atividade 1/aluno.js
+++ atividade 1/aluno.js
@@ -46,36 +46,43 @@ routerAluno.post('/',(req,res)=>{
         }
     })
 })
-routerAluno.delete('/:id',(req,res)=>{
-    const id=req.params.id;
-    const sql='DELETE * FROM tbaluno WHERE NuMatricula =?';
-    connection.query(sql,[id],(erro,linhas)=>{
-        if(erro){
-            console.log(erro)
-        }else{
-            if(linhas.length>0){
-                res.json(linhas);
-            }else{
-                res.status(404).send('Registro não localizado')
+routerAluno.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = 'DELETE FROM tbaluno WHERE NuMatricula = ?';
+    dbconn.query(sql, [id], (erro, linhas) => {
+        if (erro) {
+            console.log(erro);
+            res.status(400).send(erro.message);
+        } else {
+            if (linhas.affectedRows > 0) {
+                res.status(200).send('Registro excluído com sucesso.');
+            } else {
+                res.status(404).send('Registro não localizado');
             }
         }
     });
 });
-routerAluno.put('/',(req,res)=>{
-    const id=req.params.id
-    const NoAluno=req.body.NoAluno
-    const TxIngresso=req.body.TxIngresso
-    const IdCurso=req.body.IdCurso
-    res.status(201).send('Aluno cadastrado com sucesso.')
-    const sql = 'UPDATE FROM tbalunmo SET NoAluno=?, TxIngresso=?,IdCurso=? WHERE NuMaticula=?'
-    connection.query(sql,[NoAluno,TxIngresso,IdCurso,id],(erro,linhas)=>{
-        if(erro){
+
+routerAluno.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const NoAluno = req.body.NoAluno;
+    const TxIngresso = req.body.TxIngresso;
+    const IdCurso = req.body.IdCurso;
+
+    const sql = `UPDATE tbaluno SET NoAluno = ?, TxIngresso = ?, IdCurso = ?
+        WHERE NuMatricula = ?`;
+    dbconn.query(sql, [NoAluno, TxIngresso, IdCurso, id], (erro, linhas) => {
+        if (erro) {
             console.log(erro);
             res.status(400).send(erro.message);
-        } else{
-            res.status(200).send('Aluno atualizado com sucesso')
+        } else {
+            if (linhas.affectedRows > 0) {
+                res.status(200).send('Aluno atualizado com sucesso.');
+            } else {
+                res.status(404).send('Registro não localizado');
+            }
         }
-    })
-})
+    });
+});
 module.exports=routerAluno
 //#endregion
--- atividade 1/app.js
+++ atividade 1/app.js
@@ -2,11 +2,45 @@ const express = require('express');
 const routerAluno=require('./aluno.js')
 const bodyParser=require('body-parser')
 const routerAutor=require('./autor.js')
-
+const jwt =require ('jsonwebtoken')
+const secretWord="ifrn2024"
 const app=express();
 app.use(bodyParser.json())
-app.use('/aluno',routerAluno)
+app.use('/aluno',verificarToken,routerAluno)
 app.use('/autor',routerAutor)
+
+function gerarToken(payload){
+    return jwt.sign(payload,secretWord,{expiresIn:20})
+}
+function verificarToken(req,res,next){
+    const token =req.headers['x-acess-token'];
+    if(!token){
+        return res.status(401).json({
+            mensagemErro:"Usuário não autenticado. Faça o login antes de chamar esse recurso"
+        })
+    }else{
+        jwt.verify(token,secretWord,(error,decoded)=>{
+            if(error){
+                return res.status(403).json({mensagemErro:'Token inválido. Faça o login novamente.'})
+            }else{
+                const nomeUsuario=decoded.nomeUsuario;
+                console.log(`Usuário ${nomeUsuario} autenticado com sucesso.`)
+                next();
+            }
+        })
+    }
+}
+app.post('/login',(req,res)=>{
+    const loginName=req.body.loginName;
+    const password=req.body.password;
+    if (loginName==="admin"&&password==="123"){
+        const payload={nomeUsuario:'Administrador'}
+        const token=gerarToken(payload);
+        res.json({acessToken:token})
+    }else{
+        res.status(403).send("Usuário ou senha inválido")
+    }
+})
 
 app.listen(3000,()=>{
     console.log('Servidor está rodando na porta 3000!')
