d5b39d60ad94bc3d842c825e63a57bdfc3ea063d
MARCOS VICTOR DE ANDRADE RIBEIRO <20231148060017@ifrn.local>
2024-08-01 15:05:57 -0300

    subindo serviço de busca de cep

--- projeto-full-stack/front-end/src/app/app.component.ts
+++ projeto-full-stack/front-end/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CepService } from './cep.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,28 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  patinho = 'quack quack';
-  contador=0;
-  incrementar():void{
-    this.contador++
-  }
-  decrementar():void{
-    this.contador--
+    patinho = 'quack quack';
+    contador=0;
+    incrementar():void{
+      this.contador++
+    };
+    decrementar():void{
+      this.contador--
+    };
+    constructor(private servico:CepService){};
+
+    consultarCEP():void{
+      this.servico.consultar('01001000').subscribe({
+        next:(retorno : any)=>{
+          console.log(retorno);
+        },
+        error:(error: any)=>{
+          console.log(error);
+        },
+        complete:()=>{
+          console.log("O cep foi encontrado");
+        }
+      })
+    
   }
 }
